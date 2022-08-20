7053e5c22ba663fb2c04b19ca98961a1ac2fd8a6
Eyelly Wu <eyelly.wu@gmail.com>
2022-08-20 11:34:15 +0800

    test(bin): 添加 v|h|xxx 命令相关测试用例

--- test/bin/index.test.ts
+++ test/bin/index.test.ts
@@ -60,4 +60,101 @@ describe('验证命令行响应命令', () => {
       expect(config).toBe(templateContent)
     })
   })
+
+  describe('查看帮助', () => {
+    type Item = [
+      string, // 用例描述
+      string, // 命令
+      'zh' | 'en',
+      '命令' | 'command',
+      '用法' | 'usage',
+    ]
+
+    const matrix: Item[] = [
+      ['help', 'help', 'zh', '命令', '用法'],
+      ['h', 'h', 'zh', '命令', '用法'],
+      ['help -L en', 'help', 'en', 'command', 'usage'],
+      ['h -L en', 'h', 'en', 'command', 'usage'],
+    ]
+
+    it.each(matrix)(
+      '是否能正确响应 %s',
+      (dscrption, command, locale, cm, usage) => {
+        // 修改命令行参数
+        changeProcessArgv(command, '', '', '-L', locale)
+        const spy = vi.spyOn(console, 'log')
+        expect(spy.getMockName()).toBe('log')
+        execCommand()
+        // 正确匹配命令
+        expect(spy).toHaveBeenCalledTimes(1)
+        // 简单验证是否正常输出
+        expect(spy).toHaveBeenLastCalledWith(expect.stringContaining(cm))
+        expect(spy).toHaveBeenLastCalledWith(expect.stringContaining(usage))
+        expect(spy).toHaveBeenLastCalledWith(expect.stringContaining('i18n'))
+      },
+    )
+  })
+
+  describe('查看版本', () => {
+    type Item = [
+      string, // 用例描述
+      string, // 命令
+      'zh' | 'en',
+      '当前版本' | 'Current version',
+    ]
+
+    const matrix: Item[] = [
+      ['version', 'version', 'zh', '当前版本'],
+      ['v', 'v', 'zh', '当前版本'],
+      ['version -L en', 'version', 'en', 'Current version'],
+      ['v -L en', 'v', 'en', 'Current version'],
+    ]
+
+    it.each(matrix)('是否能正确响应 %s', (dscrption, command, locale, tip) => {
+      // 修改命令行参数
+      changeProcessArgv(command, '', '', '-L', locale)
+      const spy = vi.spyOn(console, 'log')
+      expect(spy.getMockName()).toBe('log')
+      execCommand()
+      // 正确匹配命令
+      expect(spy).toHaveBeenCalledTimes(1)
+      // 简单验证是否正常输出
+      expect(spy).toHaveBeenLastCalledWith(
+        '\n',
+        expect.stringContaining(tip),
+        '\n',
+      )
+      expect(spy).toHaveBeenLastCalledWith(
+        '\n',
+        expect.stringContaining(require('../../package.json').version),
+        '\n',
+      )
+    })
+  })
+
+  describe('命令错误', () => {
+    type Item = [
+      string, // 用例描述
+      string, // 命令
+      'zh' | 'en',
+      '输入命令有误' | 'Error in input command',
+    ]
+
+    const matrix: Item[] = [
+      ['xxx', 'xxx', 'zh', '输入命令有误'],
+      ['xxx -L en', 'xxx', 'en', 'Error in input command'],
+    ]
+
+    it.each(matrix)('是否能正确响应 %s', (dscrption, command, locale, tip) => {
+      // 修改命令行参数
+      changeProcessArgv(command, '', '', '-L', locale)
+      const spy = vi.spyOn(console, 'log')
+      expect(spy.getMockName()).toBe('log')
+      execCommand()
+      // 正确匹配命令
+      expect(spy).toHaveBeenCalledTimes(1)
+      // 简单验证是否正常输出
+      expect(spy).toHaveBeenLastCalledWith(expect.stringContaining(tip))
+    })
+  })
 })
