f9f55ae80bc985c37081ad3b8e62b063abbcb1bc
Eyelly Wu <eyelly.wu@gmail.com>
2023-04-30 23:44:30 +0800

    chore(bin): adjust init config template

--- template/i18nrc.js
+++ template/i18nrc.js
@@ -1,16 +1,16 @@
-const path = require('path')
+const { join } = require('path')
 
 module.exports = {
   funcName: 't',
-  entry: path.join(__dirname, './src/'),
+  entry: join(__dirname, './src/'),
   fileRegExp: /\.[jt]s$/,
   output: {
-    path: path.join(__dirname, './i18n/'),
+    path: join(__dirname, './i18n/'),
   },
   translator: 'googlex',
   googlexConfig: {
-    from: 'zh-CN',
-    to: ['en', 'ja'],
+    from: 'en',
+    to: ['zh-CN', 'ja'],
     codeLocaleMap: {
       ja: 'jp',
     },
