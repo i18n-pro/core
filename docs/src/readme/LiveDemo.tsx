fa418accf45aa06a6f66463c45b3bda8b18393c8
Eyelly Wu <eyelly.wu@gmail.com>
2023-06-01 23:19:01 +0800

    docs: adjust readme `Live Demo` link

--- docs/src/readme/LiveDemo.tsx
+++ docs/src/readme/LiveDemo.tsx
@@ -1,17 +1,29 @@
-import { Break, H1, UnorderedList, ListItem, Link, Image } from 'jsx-to-md'
+import { H1, UnorderedList, ListItem, Link, Image } from 'jsx-to-md'
 
 export default function LiveDemo() {
+  const localeSuffixMap = {
+    zh: 'zh-CN',
+    en: '',
+  }
+
+  const suffix = localeSuffixMap[global.docLocale]
+
+  const filename = `README${suffix ? `_${suffix}` : ''}.md`
   return (
     <>
       <H1>Live Demo</H1>
       <UnorderedList>
         <ListItem>
-          <Link href="https://codesandbox.io/p/github/eyelly-wu/i18n-pro-react-demo/main">
+          <Link
+            href={`https://codesandbox.io/p/github/eyelly-wu/i18n-pro-react-demo/main?file=${filename}`}
+          >
             Open in CodeSandbox
           </Link>
         </ListItem>
         <ListItem>
-          <Link href="https://stackblitz.com/edit/i18n-pro-react-demo">
+          <Link
+            href={`https://stackblitz.com/edit/i18n-pro-react-demo?file=${filename}`}
+          >
             <Image
               alt="Open in StackBlitz"
               title="Open in StackBlitz"
